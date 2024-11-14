8dd90b98be177ae0603dcfc8f51eebd81037884b
Allison <areel@smith.edu>
2024-11-14 11:17:57 -0500

    strided histo

--- stridedGPUhistogram.cu
+++ stridedGPUhistogram.cu
@@ -1,33 +1,65 @@
 #include <iostream>
 #include <math.h>
 #include <sys/time.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-#define TILE_WIDTH 50
+#define BLOCK_SIZE 128
+#define size 10
+#define buckets 2
 
-double get_clock() {
-        struct timeval tv; int ok;
-        ok = gettimeofday(&tv, (void *) 0);
-        if (ok<0){
-                printf("gettimeofday error");
-        }
-        return (tv.tv_sec * 1.0 + tv.tv_usec * 1.0E-6);
+__global__ void hist(unsigned char*array, unsigned int*histo){
+	int i = blockIdx.x*blockDim.x+threadIdx.x;
+	int stride = blockDim.x * gridDim.x;
+
+	if (i>=stride){
+		return;
+	}
+	while (i<size){
+		int value = array[i];
+		int bin = value % buckets;
+		atomicAdd(&(histo[bin]), 1);
+		i+=stride;
+	}
 }
 
-
 int main(){
-double *times = (double *)malloc(sizeof(double)*width);
-	 //calibrate clock
-        double t0 = get_clock();
-        for (int i=0; i<width; i++){
-            times[i] = get_clock();
-        }
-        double t1 = get_clock();
-        printf("time per call: %f nx\n", (1000000000.0 * (t1-t0\
-)/width));
+	unsigned char* array = (unsigned char*)malloc(sizeof(char)*size);
+	unsigned int* histo = (unsigned int*)malloc(sizeof(int)*size);
 
-//print clock times
-        printf("start: %f, end: %f\n", start_time, end_time);
 
-free(times);
+	for (int i=0;i<size;i++){
+		array[i]=i;
+	}
+	printf("\n");
+	unsigned char*d_array;
+	cudaMalloc(&d_array, size);
+	cudaMemcpy(d_array, array, size, cudaMemcpyHostToDevice);
+
+	unsigned int*d_histo;
+	cudaMalloc(&d_histo, buckets*sizeof(int));
+	cudaMemset(d_histo,0,buckets*sizeof(int));
+
+	dim3 block(32);
+	dim3 grid((size+block.x-1)/block.x);
+
+	cudaDeviceSynchronize();
+	hist<<<grid, block>>>(d_array, d_histo);
+	cudaDeviceSynchronize();
+	printf("%s\n", cudaGetErrorString(cudaGetLastError()));
+	cudaMemcpy(histo, d_histo, buckets*sizeof(int), cudaMemcpyDeviceToHost);
+
+	for (int i=0;i<buckets;i++) {
+		printf("%d ", histo[i]);
+	}
+	printf("\n");
+
+	free(array);
+	free(histo);
+	cudaFree(d_array);
+	cudaFree(d_histo);
+	
+	
+	
 return 0;
 }
